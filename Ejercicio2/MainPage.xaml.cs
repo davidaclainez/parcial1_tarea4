d0458c9d066ea14a510bd199402d0df9631564a0
David Castillo <43655920+davidaclainez@users.noreply.github.com>
2022-06-26 15:33:59 -0600

    tarea4v1

---
+++ Ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviarInformacion_Clicked(object sender, EventArgs e)
+        {
+            var payloadModel = new Model
+            {
+                Nombre = txtNombre.Text,
+                Apellidos = txtApellidos.Text,
+                Edad = txtEdad.Text,
+                Correo = txtCorreo.Text
+            } ;
+
+            Page page = new SecondPage();
+            page.BindingContext = payloadModel;
+            Navigation.PushAsync(page);
+        } 
+    }
+
+    class Model
+    {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Edad { get; set; }
+        public string Correo { get; set; }
+    }
+}
